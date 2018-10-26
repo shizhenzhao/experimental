0b7d7b10e79b14f201dbc812b1c7f80e66449c0a
Shizhen Zhao <shizhenzhao@shizhenzhao-macbookpro.roam.corp.google.com>
2018-10-26 07:42:05 -0700

    analysis.sh

---
+++ EV/result/analysis.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+>peak
+for scale in 1.0 1.05 1.1 1.15 1.2 1.25 1.3;
+do
+  filename=(scale=$scale)
+  finish_ratio_greedy=$(grep Greedy $filename | awk '{print $6}')
+  peak_greedy=$(grep Greedy $filename | awk '{print $8}')
+  finish_ratio_eps=$(grep Eps $filename | awk '{print $6}')
+  peak_eps=$(grep Eps $filename | awk '{print $8}')
+  finish_ratio_offline=$(grep Offline $filename | awk '{print $6}')
+  peak_offline=$(grep Offline $filename | awk '{print $8}')
+  echo $filename >> peak
+  echo $peak_greedy >> peak
+  echo $peak_eps >> peak
+  echo $peak_offline >> peak
+done
